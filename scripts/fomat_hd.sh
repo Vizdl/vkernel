dbc432521ba4e41dc9ccd3c3113448bb1ac68280
vizdl <vizdl@qq.com>
2022-08-18 17:44:49 +0800

    修复格式化分区时无法找到 loop 设备的问题

--- scripts/fomat_hd.sh
+++ scripts/fomat_hd.sh
@@ -4,16 +4,9 @@ os=$2
 cfg1=$3
 cfg2=$4
 mountdir=/tmp/mountdir
-# 1. 虚拟化虚拟硬盘为块设备
-dev=`losetup --partscan --show --find $dest`
-# 2. 查找分区块设备名
-subdev="${dev}p1" # 如若利用 ls 访问 $subdev 则可能不存在
-#echo "dev is : $dev"
-#echo "subdev is : $subdev"
-# echo "mountdir is : $mountdir"
-# 格式化分区
-mkfs.ext2 $subdev
-# 3. 挂载分区
+# 1. 虚拟化虚拟硬盘为块设备并格式化分区(这里用 && 是为了等待前面执行结束)
+dev=`losetup --partscan --show --find $dest` && subdev="${dev}p1" && mkfs.ext2 $subdev
+# 2. 挂载分区
 if [[ ! -d $mountdir ]];
 then 
     mkdir $mountdir
@@ -21,15 +14,14 @@ else
     umount $mountdir
 fi
 mount $subdev $mountdir
-# 4. 安装 grub
+# 3. 安装 grub
 grub2-install --force --no-floppy  --root-directory=$mountdir $dev
-# 5. 安装配置文件与os
+# 4. 安装配置文件与os
 cp $os $mountdir/boot
 mkdir $mountdir/etc/default -p
 cp $cfg1 $mountdir/etc/default/
 cp $cfg2 $mountdir/boot/grub2/grub.cfg
-# 6. 卸载分区
-# tree $mountdir
+# 5. 卸载分区
 umount $mountdir
 rm -rf $mountdir
 # 7. 卸载磁盘块设备
