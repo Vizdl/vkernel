2ec3057024a45b5391c7c8fa78dc5521597252f8
vizdl <vizdl@qq.com>
2022-09-19 14:40:41 +0800

    添加 irq_cpustat_t

---
+++ include/vkernel/irq_cpustat.h
@@ -0,0 +1,20 @@
+#ifndef __irq_cpustat_h
+#define __irq_cpustat_h
+
+#include <vkernel/config.h>
+
+// 对于每个 cpu 里中断信息的记载。
+extern irq_cpustat_t irq_stat[];			/* defined in asm/hardirq.h */
+
+#define __IRQ_STAT(cpu, member)	((void)(cpu), irq_stat[0].member)
+
+  /* arch independent irq_stat fields */
+#define softirq_active(cpu)	__IRQ_STAT((cpu), __softirq_active)
+#define softirq_mask(cpu)	__IRQ_STAT((cpu), __softirq_mask)
+#define local_irq_count(cpu)	__IRQ_STAT((cpu), __local_irq_count)
+#define local_bh_count(cpu)	__IRQ_STAT((cpu), __local_bh_count)
+#define syscall_count(cpu)	__IRQ_STAT((cpu), __syscall_count)
+  /* arch dependent irq_stat fields */
+#define nmi_count(cpu)		__IRQ_STAT((cpu), __nmi_count)		/* i386, ia64 */
+
+#endif	/* __irq_cpustat_h */
---
+++ kernel/softirq.c
@@ -0,0 +1,4 @@
+#include <vkernel/irq.h>
+#include <vkernel/threads.h>
+
+irq_cpustat_t irq_stat[NR_CPUS];
