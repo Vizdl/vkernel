8bc7f2fb8c6205b5ca993536f4bad0ce8981df0c
dinglei <vizdl@qq.com>
2022-12-25 21:52:35 +0800

    添加 tasklet 机制

--- include/vkernel/interrupt.h
+++ include/vkernel/interrupt.h
@@ -2,8 +2,25 @@
 #define _LINUX_INTERRUPT_H
 
 #include <asm/ptrace.h>
+#include <asm/bitops.h>
 #include <asm/softirq.h>
 #include <asm/hardirq.h>
+#include <vkernel/threads.h>
+#include <vkernel/spinlock.h>
+
+enum
+{
+	TASKLET_STATE_SCHED,	/* Tasklet is scheduled for execution */
+	TASKLET_STATE_RUN		/* Tasklet is running (SMP only) */
+};
+
+enum
+{
+	HI_SOFTIRQ=0,
+	NET_TX_SOFTIRQ,
+	NET_RX_SOFTIRQ,
+	TASKLET_SOFTIRQ
+};
 
 struct irqaction {
 	void (*handler)(int, void *, struct pt_regs *);
@@ -28,15 +45,86 @@ static inline void __cpu_raise_softirq(int cpu, int nr)
 struct tasklet_struct
 {
 	struct tasklet_struct *next;
-	unsigned long state;
-	atomic_t count;
-	void (*func)(unsigned long);
-	unsigned long data;
+	unsigned long state;			// 标志 tasklet 的状态
+	atomic_t count;					// 0 表示可执行,非 0 表示不可执行
+	void (*func)(unsigned long);	// tasklet 执行回调函数
+	unsigned long data;				// tasklet 执行上下文
 };
 
 struct tasklet_head
 {
-	struct tasklet_struct *list;
+	struct tasklet_struct *list;	// tasklet_struct 列表
 } __attribute__ ((__aligned__(SMP_CACHE_BYTES)));
+
+
+extern struct tasklet_head tasklet_vec[NR_CPUS];
+
+
+// tasklet 操作宏
+#define tasklet_trylock(t) 1
+#define tasklet_unlock_wait(t) do { } while (0)
+#define tasklet_unlock(t) do { } while (0)
+
+/**
+ * @brief 初始化指定 tasklet
+ * 
+ * @param t 待初始化的 tasklet
+ * @param func tasklet 回调函数
+ * @param data tasklet 回调函数上下文
+ */
+extern void tasklet_init(struct tasklet_struct *t,
+			 void (*func)(unsigned long), unsigned long data);
+
+/**
+ * @brief 将 tasklet_struct 添加到 tasklet_vec 中,并唤醒软中断
+ * 
+ * @param t 待调度的 tasklet
+ */
+static inline void tasklet_schedule(struct tasklet_struct *t)
+{
+	if (!test_and_set_bit(TASKLET_STATE_SCHED, &t->state)) {
+		int cpu = smp_processor_id();
+		unsigned long flags;
+		// 将 tasklet 加入 tasklet_vec 对应位置并调度软中断
+		local_irq_save(flags);
+		t->next = tasklet_vec[cpu].list;
+		tasklet_vec[cpu].list = t;
+		__cpu_raise_softirq(cpu, TASKLET_SOFTIRQ);
+		local_irq_restore(flags);
+	}
+}
+
+extern void tasklet_kill(struct tasklet_struct *t);
+
+/**
+ * @brief 禁用 tasklet : 通过增加 count 计数器
+ * 
+ * @param t 被禁用的 tasklet
+ */
+static inline void tasklet_disable_nosync(struct tasklet_struct *t)
+{
+	atomic_inc(&t->count);
+}
+
+/**
+ * @brief 禁用 tasklet : 通过增加 count 计数器
+ * 
+ * @param t 被禁用的 tasklet
+ */
+static inline void tasklet_disable(struct tasklet_struct *t)
+{
+	tasklet_disable_nosync(t);
+	tasklet_unlock_wait(t);
+}
+
+/**
+ * @brief 启用 tasklet : 通过减少 count 计数器
+ * 
+ * @param t 被启用的 tasklet
+ */
+static inline void tasklet_enable(struct tasklet_struct *t)
+{
+	atomic_dec(&t->count);
+}
 
 #endif
--- kernel/softirq.c
+++ kernel/softirq.c
@@ -3,15 +3,20 @@
 #include <vkernel/smp.h>
 #include <vkernel/init.h>
 #include <vkernel/types.h>
+#include <vkernel/sched.h>
 #include <vkernel/kernel.h>
 #include <vkernel/threads.h>
 #include <vkernel/interrupt.h>
 
 irq_cpustat_t irq_stat[NR_CPUS];
 
+// 全局 softirq 列表, 一维, 下标表示软中断向量号
 static struct softirq_action softirq_vec[32] __cacheline_aligned;
 
 static spinlock_t softirq_mask_lock = SPIN_LOCK_UNLOCKED;
+
+// 全局 tasklet 队列, 二维, 第一维是 cpu, 第二维是 tasklet
+struct tasklet_head tasklet_vec[NR_CPUS] __cacheline_aligned;
 
 /**
  * @brief 注册软中断
@@ -92,13 +97,91 @@ retry:
 }
 
 /**
+ * @brief 初始化 tasklet
+ * 
+ * @param t 待初始化的 tasklet
+ * @param func 回调函数
+ * @param data 回调函数参数
+ */
+void tasklet_init(struct tasklet_struct *t,
+		  void (*func)(unsigned long), unsigned long data)
+{
+	t->func = func;
+	t->data = data;
+	t->state = 0;
+	atomic_set(&t->count, 0);
+}
+
+/**
+ * @brief 停止 tasklet(其实本质是加速执行)
+ * 
+ * @param t 待停止的 tasklet
+ */
+void tasklet_kill(struct tasklet_struct *t)
+{
+	if (in_interrupt())
+		printk("Attempt to kill tasklet from interrupt\n");
+	// 设置任务状态为待调度
+	while (test_and_set_bit(TASKLET_STATE_SCHED, &t->state)) {
+		current->state = TASK_RUNNING;
+		do {
+			current->policy |= SCHED_YIELD;
+			// 通过调度程序使其执行软中断
+			schedule();
+		} while (test_bit(TASKLET_STATE_SCHED, &t->state)); // 直到 tasklet 真正被执行
+	}
+	tasklet_unlock_wait(t);
+	// 清除标志位
+	clear_bit(TASKLET_STATE_SCHED, &t->state);
+}
+
+
+/**
+ * @brief 执行 tasklet_vec 对应当前 cpu 的 tasklet 链表上的 tasklet
+ * 
+ * @param a 无意义的参数,实际上会被传入 NULL
+ */
+static void tasklet_action(struct softirq_action *a)
+{
+	int cpu = smp_processor_id();
+	struct tasklet_struct *list;
+	// 1. 根据 cpu 选择 取出对应的 tasklet list
+	local_irq_disable();
+	list = tasklet_vec[cpu].list;
+	tasklet_vec[cpu].list = NULL;
+	local_irq_enable();
+	// 遍历 tasklet list
+	while (list != NULL) {
+		struct tasklet_struct *t = list;
+
+		list = list->next;
+		// 2. 锁住 tasklet 并执行
+		if (tasklet_trylock(t)) {
+			if (atomic_read(&t->count) == 0) {
+				// 去除等待调度标志,表示已经执行
+				clear_bit(TASKLET_STATE_SCHED, &t->state);
+				// 真正地执行 tasklet
+				t->func(t->data);
+				tasklet_unlock(t);
+				continue;
+			}
+			tasklet_unlock(t);
+		}
+		// 如若锁失败, 将 tasklet 还回 tasklet list 末尾,等待下一次调度
+		local_irq_disable();
+		t->next = tasklet_vec[cpu].list;
+		tasklet_vec[cpu].list = t;
+		__cpu_raise_softirq(cpu, TASKLET_SOFTIRQ);
+		local_irq_enable();
+	}
+}
+
+
+/**
  * @brief 初始化软中断
  * 
  */
 void __init softirq_init(void)
 {
-
+	open_softirq(TASKLET_SOFTIRQ, tasklet_action, NULL);
 }
-
-
-struct tasklet_head tasklet_vec[NR_CPUS] __cacheline_aligned;
