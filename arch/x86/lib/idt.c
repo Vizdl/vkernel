a18b00bcfa20ec72fc99f0b4124d92841150ad95
dinglei <vizdl@qq.com>
2022-12-27 16:03:22 +0800

    将门描述符中的门类型作为宏定义

--- arch/x86/lib/idt.c
+++ arch/x86/lib/idt.c
@@ -1,25 +1,29 @@
 #include <asm/desc.h>
 
+#define CALL_TAGE_TAG 12
+#define INTR_TAGE_TAG 14
+#define TRAP_TAGE_TAG 15
+
 struct idt_desc idt_table[IDT_TABLE_SIZE] = { {0, 0}, };
 
 void set_intr_gate(unsigned int n, void *addr)
 {
-	_set_gate(idt_table+n,14,0,addr);
+	_set_gate(idt_table+n, INTR_TAGE_TAG, 0,addr);
 }
 
 
 void set_trap_gate(unsigned int n, void *addr)
 {
-	_set_gate(idt_table+n,15,0,addr);
+	_set_gate(idt_table+n, TRAP_TAGE_TAG, 0,addr);
 }
 
 
 void set_system_gate(unsigned int n, void *addr)
 {
-	_set_gate(idt_table+n,15,3,addr);
+	_set_gate(idt_table+n, TRAP_TAGE_TAG, 3,addr);
 }
 
 void set_call_gate(void *a, void *addr)
 {
-	_set_gate(a,12,3,addr);
+	_set_gate(a, CALL_TAGE_TAG, 3,addr);
 }
